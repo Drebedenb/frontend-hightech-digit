dcd686fda848828aaa7a769c78422419cd52de6e
Danila <danila.kuzya2001@gmail.com>
2022-12-05 16:15:13 +0300

    Add new (without bug) form of registration

--- frontend-hightech-digit/src/pages/Login/Login.jsx
+++ frontend-hightech-digit/src/pages/Login/Login.jsx
@@ -1,36 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, {useState} from 'react';
 import logo from '../../images/logo.png';
+import "./login.css";
 
 const Login = () => {
     let [login, setLogin] = useState("");
     let [password, setPassword] = useState("");
     return (
-        <body className="text-center">
-        <main className="form-signin w-100 m-auto">
-            <form>
-                <img className="mb-4" src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"/>
-                <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
-
-                <div className="form-floating">
-                    <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
-                    <label htmlFor="floatingInput">Email address</label>
-                </div>
-                <div className="form-floating">
-                    <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
-                    <label htmlFor="floatingPassword">Password</label>
-                </div>
-
-                <div className="checkbox mb-3">
-                    <label>
-                        <input type="checkbox" value="remember-me"/> Remember me
-                    </label>
-                </div>
-                <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-                <p className="mt-5 mb-3 text-muted">© 2017–2022</p>
-            </form>
-        </main>
-        </body>
         // <div className="text-center">
         //     <main className="form-signin w-100 m-auto">
         //         <form>
@@ -70,10 +46,43 @@ const Login = () => {
         //                 {password}
         //             </div>
         //             <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-        // {/*            <p className="mt-5 mb-3 text-muted">© 2017–2022</p>*/}
-        // {/*        </form>*/}
-        // {/*    </main>*/}
-        // {/*</div>*/}
+        //             <p className="mt-5 mb-3 text-muted">© 2017–2022</p>
+        //         </form>
+        //     </main>
+        // </div>
+
+        <div className="Auth-form-container">
+            <form className="Auth-form">
+                <div className="Auth-form-content">
+                    <img className="rounded mx-auto d-block" src={logo} alt=""/>
+                    <h3 className="Auth-form-title">Sign In</h3>
+                    <div className="form-group mt-3">
+                        <label>Email address</label>
+                        <input
+                            type="email"
+                            className="form-control mt-1"
+                            placeholder="Enter email"
+                        />
+                    </div>
+                    <div className="form-group mt-3">
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            className="form-control mt-1"
+                            placeholder="Enter password"
+                        />
+                    </div>
+                    <div className="d-grid gap-2 mt-3">
+                        <button type="submit" className="btn btn-primary">
+                            Submit
+                        </button>
+                    </div>
+                    <p className="forgot-password text-right mt-2">
+                        Forgot <a href="#">password?</a>
+                    </p>
+                </div>
+            </form>
+        </div>
     );
 };
 
