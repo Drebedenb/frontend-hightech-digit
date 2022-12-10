6cdf2e7278808b3d2a641cd667f4c1b1eab508b1
Danila <danila.kuzya2001@gmail.com>
2022-12-10 22:00:16 +0300

    Iteration through array of products and visualization

--- frontend-hightech-digit/src/pages/Main.js
+++ frontend-hightech-digit/src/pages/Main.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from "../components/Header/Header";
 import Card from "../components/Card/Card";
 import Carousel from "../components/carousel/Carousel";
 
 const Main = () => {
+    let [cards, setCards] = useState([
+        {id: 1, name: "phone", price: 500},
+        {id: 2, name: "notebook", price: 1000},
+        {id: 3, name: "pc", price: 1000},
+        {id: 4, name: "digital watch", price: 200}
+    ]);
+
     return (
         <div>
             <Header/>
             <Carousel/>
-            <Card/>
-            <Card/>
-            <Card/>
+            {(cards.map(item => <Card post={item} key={item.id}/> ) ) }
         </div>
     );
 };
